64a05c47a1f5bc5e0b7eef9fe468f081767936e8
tomhommola <97624338+tomhommola@users.noreply.github.com>
2023-07-03 11:52:41 +0200

    Test on index.js

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 // Change the text between the quotation marks and run your code to see the name of your favorite language animated!
-drawText('Here you can find my advanced courses');
+drawText('Hereyoucanfindmyadvancedcourses');
 
 // The code below will allow you to interact with the letters
 bounceBubbles();
